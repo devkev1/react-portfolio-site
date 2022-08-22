84afa45cb7c28080ca83e26599c5c9cb9722e442
Kevin Wong <kevingywong@gmail.com>
2022-08-21 21:14:45 -0400

    fixed media query and styling

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,18 +1,31 @@
 import "./project.css";
 import GitHub from "../../img/github.png";
+import { useContext } from "react";
+import { ThemeContext } from "../../context";
 
 const Project = ({ img, link, description, github }) => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
     <div className="p">
-      <a href={link} target="_blank" rel="noreferrer">
-        <div className="p-img" style={{ backgroundImage: `url(${img})` }}></div>
-      </a>
+      <div className="p-img" style={{ backgroundImage: `url(${img})` }}>
+        <a href={link} target="_blank" rel="noreferrer" className="p-site-link">
+          &nbsp;
+        </a>
+      </div>
       <p className="p-description">{description}</p>
-      <a href={github} target="_blank" rel="noreferrer">
-        <div>
-          <img src={GitHub} alt="" className="p-github" />
-        </div>
-      </a>
+      <div className="p-padding"></div>
+      <div>
+        <a
+          href={github}
+          target="_blank"
+          rel="noreferrer"
+          className="p-github-link"
+        >
+          <img src={darkMode ? GitHub : GitHub} alt="" className="p-github" />
+        </a>
+      </div>
     </div>
   );
 };
